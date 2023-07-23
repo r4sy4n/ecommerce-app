56be383fe82013a00c8dd4a9e96dc7c34dca1eb3
r4sy4n <russell.ramiro@gmail.com>
2023-07-23 15:54:29 +0800

    Feat: added productRoutes

--- client/src/products.js
+++ client/src/products.js
@@ -1,6 +1,5 @@
 const products = [
     {
-        _id:1,
         productName: '8Enhances',
         price: 2998,
         description: "Brighten up your skin, the healthy way with Gluta Power Boost 8ENHANCES! Each capsule is a synergistic blend that helps the body rejuvenate its youthful glow through free radical scavenging and removal of toxins, while improving the bodys vital organs such as the liver, skin and immune system, making it an all-in-one capsule!",
@@ -11,7 +10,6 @@ const products = [
         numOfReviews: 1
     },
     {
-        _id:2,
         productName: "8Brightens",
         price: 780,
         description: "Bring out a natural glow with 8Brightens Lightening Lotion. Infused with SPF30 and active lightening and moisturizing agents, each application is sure to bring out a healthy and natural looking glow while protecting your skin from harmful UV rays of the sun! It also leaves a light matte finish, perfect for on the go beauty regimens!",
@@ -22,7 +20,6 @@ const products = [
         numOfReviews: 1
     },
     {
-        _id:3,
         productName: "8Radiates",
         price: 298,
         description: "Show off that radiant glow with the help of 8Radiates Kojic Acid Light Soap. Specifically developed to micro-exfoliate and lighten your skin from the outside with the help of 8 unique whitening ingredients and nourishing agents including Aloe Vera, Hyaluronic Acid, and Vitamin E, you can shed those dark spots and imperfections goodbye and say hello to even, mousturized and brighter skin.",
@@ -33,7 +30,6 @@ const products = [
         numOfReviews: 1
     },
     {
-        _id:4,
         productName: "8Restores",
         price: 550,
         description: "Experience the invigorating power of 8Restores Night Repair Cream. Infused with the nourishing and renewing sensation from Active Collagen, Aloe Vera, Hyaluronic Acid, and Vitamin E, this will leave your skin rehydrated and restored to its youthful, more glowing state every morning.",
--- index.js
+++ index.js
@@ -10,11 +10,16 @@ const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(`${MONGO_URI}`);
 
+const baseURL = '/api/v1';
+const ProductRoutes = require('./routes/ProductRoutes');
+
 const app = express();
 app.use( bodyParser.json() );
 app.use( cors() );
 app.use( morgan('dev') );
 app.use( helmet() );
+
+app.use( `${baseURL}/products`, ProductRoutes );
 
 app.get( '/', ( request, response ) => {
     response.send({ message: `Express server for Ecommerce App V2`});
